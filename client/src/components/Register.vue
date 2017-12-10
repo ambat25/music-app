cf8a7dc67d8139d1c8b71d9cf2e7e5e739e83098
ambat <25ambat25@gmail.com>
2017-12-10 08:44:47 +0100

    making connection between the backend and the frontend using axios

---
+++ client/src/components/Register.vue
@@ -0,0 +1,40 @@
+
+<template>
+  <div >
+    <h2> Register </h2>
+    <!-- <p > kj </p> -->
+    <div>
+      <input type="email" name='email' v-model='email' placeholder="email"><br>
+      <input type="password" name='password' v-model='password' placeholder="password"><br>
+      <input @click="register" type="submit" value="submit">
+    </div>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      // msg: 'Welcome to Your Vue.js App'
+      email: 'ambat@live.com',
+      password: '12345'
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+      // console.log('clicked')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -4,19 +4,15 @@ var cors = require("cors");
 var morgan = require("morgan");
 
 var app = express();
-var port = 8081;
 
 app.use(morgan("combined"));
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/stat",function(req,res){
-    res.json({name:"abdul"});
+app.post("/register",function(req,res){
+    res.json(req.body);
 })
-
-// app.listen(8080);
-// app.listen(8080);
 
 app.listen(process.env.port|| 8080,function(){
     console.log("");
